fa83a65f13cf4fe004b69339abcb0101b3d33345
leulsm <leulsolm19@gmail.com>
2024-03-01 00:58:37 +0300

    controller, Exam Conrdinator and Student

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\ExamCoordinator\ExamCoordinatorController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Student\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,6 +35,18 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth', 'role:admin'])->group(function () {
     // Routes accessible to users with the 'admin' role
     Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+});
+
+// examCoordinator
+Route::middleware(['auth', 'role:examCoordinator'])->group(function () {
+    // Routes accessible to users with the 'admin' role
+    Route::get('examCoordinator/dashboard', [ExamCoordinatorController::class, 'dashboard'])->name('examCoordinator.dashboard');
+});
+
+// student
+Route::middleware(['auth', 'role:student'])->group(function () {
+    // Routes accessible to users with the 'admin' role
+    Route::get('student/dashboard', [StudentController::class, 'dashboard'])->name('student.dashboard');
 });
 
 require __DIR__ . '/auth.php';
