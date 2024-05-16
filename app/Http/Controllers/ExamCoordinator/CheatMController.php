8861fb235efe6933caa5460fcd83d35dbde0ec05
leulsm <leulsolm19@gmail.com>
2024-05-16 17:53:29 +0300

    controll

--- app/Http/Controllers/ExamCoordinator/CheatMController.php
+++ app/Http/Controllers/ExamCoordinator/CheatMController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\ExamCoordinator;
 
 use App\Http\Controllers\Controller;
+use App\Models\CheatReport;
+use App\Models\ExamSetup;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheatMController extends Controller
 {
@@ -13,22 +16,48 @@ class CheatMController extends Controller
     public function index()
     {
         //
+        $user = Auth::user(); // Assuming you're using Laravel's built-in authentication
+
+        $examSetups = ExamSetup::whereHas('cheatReports')
+            ->where('exam_coordinator_id', $user->id)
+            ->orderBy('updated_at', 'desc')
+            ->get();
+        return view('examCoordinator.cheatReport.index', compact('examSetups'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
         //
+        $examSetupId = $request->query('examSetupId');
+        // $invigilator = Invigilator::where('exam_setup_id', $examSetupId)->orderBy('updated_at', 'desc')->get();
+        $cheatReports = CheatReport::with('student')
+            ->where('exam_setup_id', $examSetupId)
+            ->orderBy('updated_at', 'desc')
+            ->get();
+        return view('examCoordinator.cheatReport.create', compact('examSetupId', 'cheatReports'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store($id)
     {
         //
+        $cheatReport = CheatReport::findOrFail($id);
+
+        // Assuming there's a relationship between CheatReport and Student
+        $student = $cheatReport->student;
+
+        // Update the student's status
+        $student->status = 'Cheated';
+        $student->save();
+
+        toastr()->success("Suspended Successfully");
+
+        return redirect()->route('cheatM.show', $cheatReport->id);
     }
 
     /**
@@ -37,6 +66,10 @@ class CheatMController extends Controller
     public function show(string $id)
     {
         //
+        $cheatreport = CheatReport::findOrFail($id);
+        $examSetupId = $cheatreport->exam_setup_id;
+
+        return view('examCoordinator.cheatreport.show', compact('cheatreport', 'examSetupId'));
     }
 
     /**
