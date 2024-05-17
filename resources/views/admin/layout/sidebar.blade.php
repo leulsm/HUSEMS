27c86e4c23b8ecc20bdda9917ae87326501d1e67
Leiul <liulmekonnen05@gmail.com>
2024-05-17 08:06:19 +0300

    profile admin

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -6,28 +6,7 @@
         </ul>
     </form>
     <ul class="navbar-nav navbar-right">
-        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
-            <div class="dropdown-menu dropdown-list dropdown-menu-right">
-              <div class="dropdown-header">Notifications
-                <div class="float-right">
-                  <a href="#">Mark All As Read</a>
-                </div>
-              </div>
-              <div class="dropdown-list-content dropdown-list-icons">
-                <a href="#" class="dropdown-item dropdown-item-unread">
-                  <div class="dropdown-item-icon bg-primary text-white">
-                    <i class="fas fa-code"></i>
-                  </div>
-                  <div class="dropdown-item-desc">
-                    Exam setup added
-                    <div class="time text-primary">2 Min Ago</div>
-                  </div>
-                </a>
-              <div class="dropdown-footer text-center">
-                <a href="#">View All <i class="fas fa-chevron-right"></i></a>
-              </div>
-            </div>
-          </li>
+        
 
         <li class="dropdown"><a href="#" data-toggle="dropdown"
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
@@ -36,13 +15,13 @@
                 <div class="d-sm-none d-lg-inline-block">Hi, {{ auth()->user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
-                {{-- <div class="dropdown-title">Logged in 5 min ago</div> --}}
-                <a href="#" class="dropdown-item has-icon">
+                 <div class="dropdown-title">Logged in 5 min ago</div> 
+                <a href="{{route('update.index')}}" class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
                 </a>
-                {{-- <a href="features-settings.html" class="dropdown-item has-icon">
+                 <a href="features-settings.html" class="dropdown-item has-icon">
                     <i class="fas fa-cog"></i> Settings
-                </a> --}}
+                </a> 
                 <div class="dropdown-divider"></div>
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
--- resources/views/examCoordinator/layout/sidebar.blade.php
+++ resources/views/examCoordinator/layout/sidebar.blade.php
@@ -77,7 +77,7 @@
                 </ul>
             </li>
             <li class="dropdown">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
                     <span><strong>Invigilator Management</strong></span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="{{ route('invigilatorManagement.index') }}"><strong>Add
